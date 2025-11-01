adfb88654bb9b8a439c2f8d9cb82c66e83f8847e
parth-2005 <pjpanot260305@gmail.com>
2025-11-01 11:45:43 +0530

    js Basics

---
+++ 01_basics/jsbasics.js
@@ -0,0 +1,20 @@
+// console.log("Hello, World!")
+
+// let namee = "Parth";
+// let age = 20;
+
+// console.log(namee)
+// console.log(age)
+
+// console.log(namee , age);
+// console.table([namee , age]);
+
+// console.log(typeof age)
+
+let a = 2;
+let b = "2";
+console.log(a == b);
+console.log(a === b);
+
+
+
